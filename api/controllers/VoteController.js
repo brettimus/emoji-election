74d8bb7bc9d68612264728cf8f389fec88911944
brettimus <brbeut@gmail.com>
2015-07-17 20:33:04 -0700

    add some (incomplete) scaffolding

---
+++ api/controllers/VoteController.js
@@ -0,0 +1,24 @@
+/**
+ * VoteController
+ *
+ * @description :: Server-side logic for managing votes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	create: function(req, res) {
+        // Grab tweet from req
+        // 
+    }
+};
+
+function parseTweet(req, res, next) {
+    // Reject if there isn't exactly one candidate mentioned
+
+    // Candidate.findByHandle(...)
+}
+
+function parseMentions(tweet) {
+
+}
+
