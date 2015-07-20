c42790725f6280d11903ea67587c33874615cb5f
brettimus <brbeut@gmail.com>
2015-07-20 04:19:31 -0700

    refactor voting explanation

--- assets/js/voting-instructions.js
+++ assets/js/voting-instructions.js
@@ -4,8 +4,13 @@ addClickEvent(votingButton, toggleVotingInstructions);
 
 function toggleVotingInstructions(evt) {
     console.log(evt);
-    var elt = document.querySelector(".voting-instructions");
-    elt.className = elt.className.replace("hidden", "") + " slideInDown";
+    var elt = document.querySelector(".voting-instructions-container");
+    if (elt.className.indexOf("hidden") !== -1) {
+        elt.className = elt.className.replace("hidden", "") + " slideInDown";
+    }
+    else {
+        elt.className = elt.className.replace("slideInDown", "") + " hidden";
+    }
 }
 
 function addClickEvent(node, handler) {
