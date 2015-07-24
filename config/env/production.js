19f7cf05eca754d49b3686d8282896e1f500c2cd
brettimus <brbeut@gmail.com>
2015-07-24 01:51:45 -0700

    configure dev and prod to use port 80

--- config/env/production.js
+++ config/env/production.js
@@ -26,13 +26,13 @@ module.exports = {
   models: {
     connection: 'postgres',
     migrate: 'safe',
-  }
+  },
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  // port: 80,
+  port: 80,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
