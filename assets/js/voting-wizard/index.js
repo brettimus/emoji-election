4614b553712cfc2dd1315e76012aadce2a72d667
brettimus <brbeut@gmail.com>
2015-07-26 01:25:44 -0700

    remove unnec comments

--- assets/js/voting-wizard/index.js
+++ assets/js/voting-wizard/index.js
@@ -12,13 +12,3 @@ module.exports = function() {
     require("./emoji")();
 };
 
-// Voting wizard transitions
-//
-// TODO - tap into that history api? it sucks if the user hits back and isn't at their form.
-//      - that'd be lots of work... maybe just... use backbone... mehmehmeh
-
-function showVotingWizard(evt) {
-    var elt = document.querySelector("[data-target='voting-wizard']");
-    removeClass(elt, "voting-wizard-container-hidden");
-}
-
